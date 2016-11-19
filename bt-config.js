547e63d43da7bad20cdefff8d0930c4de96fb93c
Mark <mark@heyimmark.com>
2016-11-19 16:06:13 -0500

    Add dist tests
    Upgrades to latest version of build-tools package that allows
    multiple test configs to be ran. We are now testing the dist
    version of the files also when performing a build.

--- bt-config.js
+++ bt-config.js
@@ -1,21 +1,34 @@
 'use strict';
-module.exports = {
-    dist: 'dist',
-    build: {
-        files: {
-            'dist/resource-manager.js': ['src/resource-manager.js']
-        },
-        browserifyOptions: {
-            standalone: 'ResourceManager'
-        },
-        minifyFiles: {
-            'dist/resource-manager-min.js': ['dist/resource-manager.js']
-        },
-        bannerFiles: ['dist/*']
-    },
-    tests: {
-        mocha: {
-            src: ['tests/*.js']
-        },
+var srcTest = {
+    mocha: {
+        files: ['tests/*.js']
     }
+};
+
+var distTest = Object.assign({}, srcTest, {
+  requires: {
+      '../src/resource-manager': './dist/resource-manager'
+  }
+});
+
+module.exports = {
+    production: {
+        build: {
+            files: {
+                'dist/resource-manager.js': ['src/resource-manager.js']
+            },
+            browserifyOptions: {
+                standalone: 'ResourceManager'
+            },
+            minifyFiles: {
+                'dist/resource-manager-min.js': ['dist/resource-manager.js']
+            },
+            bannerFiles: ['dist/*'],
+        },
+        tests: [srcTest, distTest]
+    },
+    development: {
+        watch: true,
+        tests: srcTest
+    },
 };

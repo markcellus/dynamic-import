4c203fd8082db8999bed741db559ac6fe95045fa
Mark <mark@heyimmark.com>
2018-09-07 12:47:29 -0400

    Convert to module

--- bt-config.js
+++ bt-config.js
@@ -8,19 +8,6 @@ const transform = [
     ]
 ];
 module.exports = {
-    build: {
-        files: {
-            'dist/resource-manager.js': ['src/resource-manager.js']
-        },
-        browserifyOptions: {
-            standalone: 'ResourceManager',
-            transform
-        },
-        minifyFiles: {
-            'dist/resource-manager-min.js': ['dist/resource-manager.js']
-        },
-        bannerFiles: ['dist/*']
-    },
     tests: {
         mocha: {
             files: ['tests/*.js'],
